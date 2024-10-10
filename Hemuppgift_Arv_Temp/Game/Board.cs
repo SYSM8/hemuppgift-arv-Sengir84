4db70546dc17110174f0fd7864d57ed4ea5f16df
Alexander Larsson <alexander.larsson@edu.newton.se>
2024-10-10 20:51:41 +0200

    Board Class grund
    Mapp och board class skapad. Attribut och metoder gjorda utan kod

---
+++ Hemuppgift_Arv_Temp/Game/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp
+{
+    internal class Board
+    {
+        //Attribut
+        public int NoPins { get; set; }
+       
+        
+        //Metoder
+        public void SetUp(int pins) 
+        { 
+        
+        }
+
+        public void TakePins(int input)
+        {
+
+        }
+
+        public int GetNoPins() 
+        { 
+            return NoPins; 
+        }
+
+    }
+}
