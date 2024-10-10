42948f32e9e2fd4c4d2d4b6048b45232f964f553
Alexander Larsson <alexander.larsson@edu.newton.se>
2024-10-10 23:16:18 +0200

    board metoder, start i main
    Set up kan kallas och ge ett värde från main. En metod för tryparse tillagd

--- Hemuppgift_Arv_Temp/Game/Board.cs
+++ Hemuppgift_Arv_Temp/Game/Board.cs
@@ -1,26 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hemuppgift_Arv_Temp
 {
-    internal class Board
+    public class Board
     {
         //Attribut
         public int NoPins { get; set; }
        
         
-        //Metoder
-        public void SetUp(int pins) 
-        { 
         
+        //Metoder
+
+        //Metod som skapar en lista med så många pins som man vill spela med
+        public void SetUp(int input) 
+        {
+            if (input > 0)
+            {
+                NoPins = input;
+                Console.WriteLine("Spelet är förberett varsågod att börja");
+            }
+            else { Console.WriteLine("det måste finnas pins"); }
+           
         }
 
         public void TakePins(int input)
         {
-
+           NoPins -= input;
+            Console.WriteLine($"Nu finns det {NoPins} kvar");
         }
 
         public int GetNoPins() 
--- Hemuppgift_Arv_Temp/Game/TakePins.cs
+++ Hemuppgift_Arv_Temp/Game/TakePins.cs
@@ -1,11 +1,41 @@
-﻿namespace Hemuppgift_Arv_Temp
+﻿using System.Threading.Channels;
+
+namespace Hemuppgift_Arv_Temp
 {
-    internal class TakePins
+    public class TakePins
     {
         //Här är main klassen där koden ska testas, lägg in i mappen
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Board board = new Board();
+            Console.WriteLine("hur många pins?");
+            
+            board.SetUp(InputHandling(Console.ReadLine()));
+            
+            
+        }
+
+
+
+
+
+
+
+
+
+        //Metod för att kontrollera att det är en siffra annars returneras noll
+        static int InputHandling(string input)
+        {
+            bool isnumber = int.TryParse(input, out int nmbr);
+            if (isnumber)
+            {
+                return nmbr;
+            }
+            else 
+            {
+                return 0;             
+            }
+            
         }
 
     }
