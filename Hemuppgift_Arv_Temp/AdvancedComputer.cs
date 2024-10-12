2af41c139cd3e236a005252b584b19418cef019c
Alexander Larsson <alexander.larsson@edu.newton.se>
2024-10-13 00:49:54 +0200

    Kommenterat AdvancedComputer

--- Hemuppgift_Arv_Temp/AdvancedComputer.cs
+++ Hemuppgift_Arv_Temp/AdvancedComputer.cs
@@ -13,8 +13,9 @@ namespace Hemuppgift_Arv_Temp
         
         public override int TakePins(Board board)
         {
+            //variabel som finns för att räkna ut vad som är delbart med 3 för att kunna lämna det till spelaren
             int leavepins = board.NoPins % 3;
-
+            // om där är en eller 2 pins kvar ska datorn ta dem
             if (board.NoPins == 1 || board.NoPins == 2)
             {
                 if (board.NoPins == 1)
@@ -28,12 +29,13 @@ namespace Hemuppgift_Arv_Temp
                     return board.NoPins;
                 }
             }
-            //om leavepins = 0 är vi i en multipel av 3
+            //om leavepins = 0 är vi i en multipel av 3 om leavepins = 1 ska datorn ta en för att komma in i en multipel av 3
             else if (leavepins == 0 || leavepins == 1)
             {
                 board.TakePins(1);
                 return board.NoPins;
             }
+            //om leavepins = 2 ska datorn ta 2 för att komma in i en multipel av 3
             else if (leavepins == 2)
             {
                 board.TakePins(2);
